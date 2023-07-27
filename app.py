5ac7dd77210e9e6e63ea42bbba459339052f405b
ckcodes907 <137890733+ckcodes907@users.noreply.github.com>
2023-07-27 13:03:37 +0000

    added json file

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask 
+from flask import render_template
+from flask import jsonify
 
 app=Flask(__name__)
-print(__name__)
+TOOLS = [
+{
+  'Name': 'Strong Password Generator'
+}
+]
 
 @app.route("/")
-def hello_world():
-  return "hello world"
+def hello_Logic():
+  return render_template('index.html', tools= TOOLS)
+
+@app.route("/api/tools")  
+def list_tools():
+  return jsonify(TOOLS)
+  
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
 
