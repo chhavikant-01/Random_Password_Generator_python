fda4b261a0014dc34ce89e9561d752c24b33fe4f
ckcodes907 <137890733+ckcodes907@users.noreply.github.com>
2023-08-07 14:48:21 +0000

    added functionality to pw generator

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask 
+from flask import Flask, request
 from flask import render_template
 from flask import jsonify
+import random, string
 
 app=Flask(__name__)
+
 TOOLS = [
 {
   'Name': 'Strong Password Generator'
@@ -12,6 +14,29 @@ TOOLS = [
 @app.route("/")
 def hello_Logic():
   return render_template('index.html', tools= TOOLS)
+
+# Function to generate a strong password
+def generate_password(length=12):
+    password = ''
+    characters = string.printable
+    for i in range(length):
+      c = ''
+      c = random.choice(characters)
+      password = password + c
+      return password
+      
+
+@app.route('/', methods=['GET', 'POST'])
+def passwordgen():
+    if request.method == 'POST':
+        print("Received POST request:")
+        password_length = int(request.form.get('password_length', 12))
+        password = generate_password(password_length)
+        return render_template('index.html', PASSWORD=password)
+    else:
+      print("NOT Received POST request:")
+      return render_template('index.html', PASSWORD='',tools = TOOLS)
+    
 
 @app.route("/api/tools")  
 def list_tools():
